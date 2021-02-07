e03d926902300f66158bd9d07c7f6421e774c2c3
Pierre L <pierre.loubateres@gmail.com>
2021-02-07 14:21:58 -0500

    changes

--- assignment1Sandwich.js
+++ assignment1Sandwich.js
@@ -2,6 +2,7 @@ const Order = require("./assignment1Order");
 
 const OrderState = Object.freeze({
     WELCOMING:   Symbol("welcoming"),
+    CHOICE:   Symbol("choice"),
     TYPE:   Symbol("type"),
     TOPPINGS:   Symbol("toppings"),
     DRINKS:  Symbol("drinks")
@@ -11,6 +12,7 @@ module.exports = class SandwichOrder extends Order{
     constructor(){
         super();
         this.stateCur = OrderState.WELCOMING;
+        this.sChoice = "";
         this.sType = "";
         this.sToppings = "";
         this.sDrinks = "";
@@ -20,6 +22,25 @@ module.exports = class SandwichOrder extends Order{
         let aReturn = [];
         switch(this.stateCur){
             case OrderState.WELCOMING:
+                this.stateCur = OrderState.CHOICE;
+                aReturn.push("Welcome to Pierre's Sandwichery.");
+                aReturn.push("Would you like a premade sandwich or custom?");
+                aReturn.push("Enter premade or custom");
+                break;
+            case OrderState.CHOICE:
+                this.sChoice = sInput;
+                if(sChoice.toLowerCase() != "premade"){
+                    this.stateCur = OrderState.TYPE;
+                    aReturn.push("What sandwich would you like?");
+                    aReturn.push("Scramble, BBQ, Teriyaki, Veggietable");
+                }
+                else if(sChoice.toLowerCase() != "custom"){
+                    this.stateCur = OrderState.TYPE;
+                    aReturn.push("What type of bread would you like?");
+                    aReturn.push("Bagel, Baguette, Multigrain, Croissant");
+                }
+                break;    
+            case OrderState.WELCOMING:
                 this.stateCur = OrderState.TYPE;
                 aReturn.push("Welcome to Pierre's Sandwichery.");
                 aReturn.push("What type of bread would you like?");
@@ -28,7 +49,8 @@ module.exports = class SandwichOrder extends Order{
             case OrderState.TYPE:
                 this.stateCur = OrderState.TOPPINGS
                 this.sType = sInput;
-                aReturn.push("What toppings would you like?");
+                aReturn.push("Pick up to 4 toppings:");
+                aReturn.push("Tomato, Spinach, Baked Tofu, Avocado, Pickled Onion, Roasted Garlic, Mushroom,");
                 break;
             case OrderState.TOPPINGS:
                 this.stateCur = OrderState.DRINKS
@@ -41,13 +63,13 @@ module.exports = class SandwichOrder extends Order{
                     this.sDrinks = sInput;
                 }
                 aReturn.push("Thank-you for your order of");
-                aReturn.push(`${this.sSize} ${this.sItem} with ${this.sToppings}`);
+                aReturn.push(`${this.sType} ${this.sItem} with ${this.sToppings}`);
                 if(this.sDrinks){
                     aReturn.push(this.sDrinks);
                 }
                 let d = new Date(); 
                 d.setMinutes(d.getMinutes() + 20);
-                aReturn.push(`Please pick it up at ${d.toTimeString()}`);
+                aReturn.push(`Please pick it up at our Restaurant on 50 William Street West Waterloo N1G 0E3 at ${d.toTimeString()}`);
                 break;
         }
         return aReturn;
